7a4bc53a0d97ab76aac367c9bb2843d450eda510
Sharvan TG <sharvantg@hotmail.com>
2024-04-15 15:15:22 +0530

    Solved TS errors

--- cost-of-ailments/src/app/(components)/Autocomplete.tsx
+++ cost-of-ailments/src/app/(components)/Autocomplete.tsx
@@ -4,8 +4,9 @@ import { createRoot } from "react-dom/client";
 
 export function Autocomplete(props: any) {
   const containerRef = useRef(null);
-  const panelRootRef = useRef(null);
-  const rootRef = useRef(null);
+  const panelRootRef = useRef<HTMLElement | null>(null);
+  //define a rootRef with useRef type as html element
+  const rootRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
     if (!containerRef.current) {
@@ -18,11 +19,12 @@ export function Autocomplete(props: any) {
       render({ children }, root) {
         if (!panelRootRef.current || rootRef.current !== root) {
           rootRef.current = root;
-
+          //@ts-ignore
           panelRootRef.current?.unmount();
+          //@ts-ignore
           panelRootRef.current = createRoot(root);
         }
-
+        //@ts-ignore
         panelRootRef.current.render(children);
       },
       ...props,
